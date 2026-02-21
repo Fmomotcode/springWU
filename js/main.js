1b597fd9b08dd08852e4a9551618fe0d2d4b03e8
Fmomotcode <filipmom10@gmail.com>
2026-02-21 21:11:19 +0100

    har lagt till lite javascript

---
+++ js/main.js
@@ -0,0 +1,19 @@
+const button = document.querySelector(".read-more");
+const text = document.querySelector(".preview");
+const img = document.querySelector(".mindre")
+
+button.addEventListener("click", () => {
+    text.classList.toggle("expanded");
+    button.textContent = 
+        text.classList.contains("expanded") 
+        ? "Visa mindre" 
+        : "Läs mer";
+});
+
+//img.addEventListener("mouseenter", () => {
+    img.classList.add("mer");
+//});
+
+//img.addEventListener("mouseleave", () => {
+    img.classList.remove("mer");
+//}); 
