f23592edc8c0a1df192b908dca7482af37d4fc98
Fmomotcode <filipmom10@gmail.com>
2026-04-10 20:11:07 +0200

    arbete hemma under påsklovet (dokumenterad i loggboken)

--- js/main.js
+++ js/main.js
@@ -5,6 +5,9 @@ const main = document.querySelector("main");
 const btnOpen = document.querySelector("#btnOpen");
 const btnClose = document.querySelector("#btnClose");
 const navLinks = document.querySelector("#nav-links");
+const slider = document.querySelector(".slider-track");
+const nextButton = document.querySelector(".next");
+const prewButton = document.querySelector(".prev")
 
 button.addEventListener("click", () => {
     text.classList.toggle("expanded");
@@ -34,3 +37,25 @@ function closeMobileMenu() {
 
 btnOpen.addEventListener('click', openMobileMenu);
 btnClose.addEventListener('click', closeMobileMenu);
+
+if (slider && nextButton && prewButton) {
+    let index = 0;
+    const cards = slider.querySelectorAll(".info-och-bilder");
+
+    function updateSlider() {
+        slider.style.transform = `translateX(-${index * 280}px)`;
+    }
+
+    nextButton.addEventListener("click", () => {
+        index++;
+        if (index > cards.length - 1) 
+            index = 0;
+        updateSlider();
+    });
+
+    prewButton.addEventListener("click", () => {
+        index--;
+        if (index < 0) index = cards.length - 1;
+        updateSlider();
+    });
+}
